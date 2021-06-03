79ea49d3d2db293ed1de7a5dd245b380e87c7cb8
asepilham <84943488+asepilham@users.noreply.github.com>
2021-06-03 23:14:16 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '1662315039:AAH1VH9K0iy28whf-EANfjU7eq0pPdLHnq0'
 const bot = new TelegramBot(token, {polling: true});
 
 
